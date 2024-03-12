70fd4cecd95cfb7ff7562114c6543eaa93692ead
albab83 <141700905+albab83@users.noreply.github.com>
2024-03-12 17:14:13 +0700

    oke selesai

--- Contact-app/app.js
+++ Contact-app/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -35,18 +34,3 @@
 // }
 
 // main();
-=======
-const contacts = require('./contacts')
-
-
-const main = async() => {
-    const nama = await contacts.tulisPertanyaan('Masukan nama anda: ')
-    const noHp = await contacts.tulisPertanyaan('Masukan No Hp anda: ')
-    const email = await contacts.tulisPertanyaan('Masukan email anda: ')
-
-
-    contacts.simpanContact(nama, noHp, email)
-}
-
-main();
->>>>>>> 80c18e2bdf95dbfeca06ef87fdb77478c51e6dcc
